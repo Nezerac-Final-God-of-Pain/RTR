86a658702df54dcb86440f53b4ba6d5abca9bcfb
Nezerac-Final-God-of-Pain <jamietindle07@gmail.com>
2025-12-09 16:41:06 +0000

    evolved changes

--- RolsaTechnologies/website/views.py
+++ RolsaTechnologies/website/views.py
@@ -1,18 +1,42 @@
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm
+from .forms import CreateUserForm,LoginForm
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 
 def home(request):
-    return render(request, 'website/index.html')
+    return render(request, 'website/homepage.html')
+
+def userdashboard(request):
+    return render(request, 'website/user.html')
 
 def register(request):
     form = CreateUserForm()
 
     if request.method == "POST":
-        form = CreateUserForm(Request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid:
             form.save
-            # return redirect
+            return redirect('')
 
     context = {'form': form}
 
     return render(request, 'website/register.html', context=context)
+
+def login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+    
+            user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('')
+
+    context = {'form': form}
+    return render(request, 'website/login.html', context = context)
